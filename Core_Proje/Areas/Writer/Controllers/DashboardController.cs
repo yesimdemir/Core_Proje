e7a59f9cc8fa1046a8a9fe7f6e1f0d3353ae7880
yesimdemir <yesimdmr000@gmail.com>
2024-02-24 17:18:58 +0300

    Asp.Net Core ile yapılan bir özgeçmiş sitesi
    Asp.Net Core ile yapılan bir özgeçmiş sitesi

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    [Route("Writer/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            //Weather APi
+            string api = "4413b4017481b7a34b57147414a9783d";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=kirklareli&appid=4413b4017481b7a34b57147414a9783d" + api;
+            //XDocument document = XDocument.Load(connection);
+            //ViewBag.v5 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+            Context c = new Context();
+            ViewBag.v1 = c.WriterMessages.Where(x => x.Receiver == values.Email).Count();
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = c.Users.Count();
+            ViewBag.v4 = c.Skills.Count();
+            return View();
+        }
+    }
+}
+/*
+ http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=14ad2aba611dbef9c504b82a127794c5
+ */
---
+++ EntityLayer/Concrete/WriterUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class WriterUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
